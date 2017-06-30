406b36e919e42db2af5c01678f8a57e632c489c8
tharajasombat <n.rajasombat@gmail.com>
2017-06-30 14:27:21 -0700

    Final Stage

---
+++ db/migrate/20170629214104_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :description
+      t.text :ingredients
+      t.text :instructions
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
